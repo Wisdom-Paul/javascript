c584662842d654bb8d471354f3607c79d79182e9
Wisdom-Paul <159251775+Wisdom-Paul@users.noreply.github.com>
2024-02-16 08:38:03 +0100

    Create scrimba.js

---
+++ scrimba.js
@@ -0,0 +1,10 @@
+// let myAge = 25;
+// console.log(myAge)
+
+let myAge= 25;
+// let humanDogRatio=7
+// let myDogAge= myAge * humanDogRatio
+// console.log(myDogAge)
+myAge = 12
+console.log(myAge)
+let bonusPoints = 50
